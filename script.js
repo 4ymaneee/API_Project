b28f5c66ce625a7d13592a3c66e3cc9efbc41466
4ymaneee <nalmlwmyat4@gmail.com>
2023-10-30 22:19:11 +0100

    Add Another Method

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Old Way
+
 //Get All Posts
 function getPosts(userId) {
   let request = new XMLHttpRequest();
@@ -33,9 +35,9 @@ function getUsers() {
     allUsers.innerHTML = "";
     for (let user of users) {
       allUsers.innerHTML += `<div class="user" onclick="userClicked(${user.id}, this)">
-                                     <h5>${user.name}</h5>
-                                     <h6>${user.email}</h6>
-                                     </div>`;
+                                 <h5>${user.name}</h5>
+                                 <h6>${user.email}</h6>
+                                 </div>`;
     }
   };
 }
@@ -43,11 +45,58 @@ function getUsers() {
 getPosts(1);
 getUsers();
 
-function userClicked(id,ele) {
+function userClicked(id, ele) {
   getPosts(id);
-  let selectedElements = document.getElementsByClassName('selected')
-  for(let element of selectedElements){
-    element.classList.remove('selected')
+  let selectedElements = document.getElementsByClassName("selected");
+  for (let element of selectedElements) {
+    element.classList.remove("selected");
   }
-  ele.classList.add('selected')
+  ele.classList.add("selected");
+}
+
+//New Way
+
+//Get All Posts
+function getAllPosts(userId) {
+  fetch("https://jsonplaceholder.typicode.com/posts?userId=" + userId)
+    .then((response) => response.json())
+    .then((posts) => {
+      let allPosts = document.querySelector(".posts");
+      allPosts.innerHTML = "";
+      for (let post of posts) {
+        allPosts.innerHTML += ` <div class="post">
+                                <h4>${post.title}</h4>
+                                <hr style="margin: 5px 0 5px 0;">
+                                <h5>${post.body}</h5>
+                                </div>`;
+      }
+    });
+}
+
+//Get All Users
+function getAllUsers() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((users) => {
+      let allUsers = document.querySelector(".users");
+      allUsers.innerHTML = "";
+      for (let user of users) {
+        allUsers.innerHTML += `<div class="user" onclick="userCLicked(${user.id}, this)">
+                                     <h5>${user.name}</h5>
+                                     <h6>${user.email}</h6>
+                                     </div>`;
+      }
+    });
+}
+
+getAllPosts(1);
+getAllUsers();
+
+function userCLicked(id, ele) {
+  getAllPosts(id);
+  let allUsers = document.querySelectorAll(".user");
+  for (let user of allUsers) {
+    user.classList.remove("selected");
+  }
+  ele.classList.add("selected");
 }
